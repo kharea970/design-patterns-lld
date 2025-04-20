a572e8d6814faaa747aebdf85c3562a0099ea73e
Ashish Khare <kharea970@gmail.com>
2025-04-20 16:34:32 +0530

    Initial commit

---
+++ src/main/java/com/example/designpatternlld/DesignPatternLldApplication.java
@@ -0,0 +1,13 @@
+package com.example.designpatternlld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternLldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternLldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/designpatternlld/DesignPatternLldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designpatternlld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternLldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
